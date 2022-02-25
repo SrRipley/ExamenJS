ebaa72e63e15fb8c05e65d7211682d69e03f87a1
Darío <dcalvor@esliceu.net>
2022-02-25 19:43:15 +0100

    examen Darío Calvo

--- index.js
+++ index.js
@@ -3,13 +3,50 @@ function _id(ident){
 }
 
 function factorial(){
+
+    var numero = _id("entradaFactorial").value;
+    var operar = "";
+    var global = 1;
     
+
+    for ( var i = 1; i <= numero; i++ ){
+        global = global * i;
+        operar += i + "*"
+    }
+    _id("operacionsFactorial").innerHTML = operar;
+    _id("resultatFactorial").innerHTML = global;
+    
+
 }
 
-function calculaIVA(){
-    
-}
+function calculaIVA(tipusIVA){
+    var preuAmbIVA = id_("preuAmbIVA");
+    var preuSenseIVA;
+        if (tipusIVA == 21) {
+                preuSenseIVA = preuAmbIVA / 1.21;
+            }else if (tipusIVA == 10){ 
+                preuSenseIVA = preuAmbIVA / 1.10;
+            }else if (tipusIVA == 4) {
+                preuSenseIVA = preuAmbIVA / 1.04;
+                }
+        preuSenseIVA = resultado.toPrecision(2); //precio sin iva y con dos decimales
+    IVA = preuAmbIVA - preuSenseIVA; //iva es el resto
+        }
+
 
 function canviaFons(tipus){
+    var colorFondo;
+    var colorTexto;
+        if (tipus == "clear"){
+            colorFondo = "white";
+            colorTexto = "black";
+    }
+        else if (tipus == "random"){
+            var gris = Math.floor(Math.random() * 255);
+            colorFondo = "rgb(" + gris + "," + gris + "," + gris + ")";
     
-}
+    
+
+
+    _id("cos").style.backgroundColor = colorFondo;
+    _id("cos").style.color = colorTexto; }}
